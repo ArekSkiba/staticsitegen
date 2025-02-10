d58c8fcb71b58bd76d77e36aaab15ab12542cb1e
ArekSkiba <arek.skiba77@gmail.com>
2025-02-10 20:04:56 +0100

    CH3 L6 text_to_textnodes copied from solution files, its A LOT easier

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,6 +1,15 @@
 import re
 from textnode import TextNode, TextType
 
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     result = []
@@ -79,12 +88,3 @@ def split_nodes_link(old_nodes):
             z = TextNode(original_text, TextType.TEXT)
             result.append(z)
     return result
-
-def text_to_textnodes(text):
-    nodes = [TextNode(text, TextType.TEXT)]
-    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
-    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
-    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
-    nodes = split_nodes_image(nodes)
-    nodes = split_nodes_link(nodes)
-    return nodes
