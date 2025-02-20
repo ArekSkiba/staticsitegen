872a153e38591902063e317baf7d466f210265c0
ArekSkiba <arek.skiba77@gmail.com>
2025-02-20 22:14:07 +0100

    CH5 L1 copy static->public recursive

--- src/main.py
+++ src/main.py
@@ -1,9 +1,38 @@
 from textnode import *
 from htmlnode import *
+import shutil
+import os
+
+static_path = './static'
+public_path = './public'
+
 
 def main():
-    node1 = TextNode("dummyvalue", TextType.BOLD, "https://www.boot.dev")
-    print(node1)
+    if not os.path.exists(static_path):
+        raise Exception('source directory does not exist')
+    if os.path.exists(public_path):
+        shutil.rmtree(public_path) 
+    os.mkdir(public_path)
+    copy_contents(static_path, public_path)
+    
+    
+def copy_contents(src_dir, dst_dir):
+    
+    elements = os.listdir(src_dir)
+    if elements:
+        for element in elements:
+            path = os.path.join(src_dir, element)
+            if os.path.isfile(path):
+                shutil.copy(path, dst_dir)
+            else:
+                new_src = os.path.join(src_dir, element)
+                new_dst = os.path.join(dst_dir, element) 
+                os.mkdir(new_dst)
+                copy_contents(new_src, new_dst)
+
+
+
+
 
 if __name__ == "__main__":
     main()
